dacd2b8da8ab6c9dc5be80752ab0764e994208e7
2021056317 <myday2day@hanyang.ac.kr>
2023-10-30 12:08:38 +0900

    swiftUI init

---
+++ Cuckoo_swiftUI/Cuckoo/Cuckoo/CuckooApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuckooApp.swift
+//  Cuckoo
+//
+//  Created by 유철민 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct CuckooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
