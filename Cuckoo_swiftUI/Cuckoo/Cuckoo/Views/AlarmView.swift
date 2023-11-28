daa60d229a64192d1efe698aff9793b5cdd9d5d9
jjangddu <peterjdw@naver.com>
2023-11-28 11:33:13 +0900

    [fix] button navigation 넣기
    알람, 설정, 메모등록 버튼 navigationlink로 넣기

---
+++ Cuckoo_swiftUI/Cuckoo/Cuckoo/Views/AlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmView.swift
+//  Cuckoo
+//
+//  Created by 장동우 on 2023/11/28.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        Text("AlarmALARMAlarmALARMAlarmALARMAlarmALARMAlarmALARMAlarmALARM")
+    }
+}
+
+#Preview {
+    AlarmView()
+}
