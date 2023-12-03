38a5825fb938a1fdfa2a0a0b7b32d863314e4823
Jiwon Pyo <dksu40@gmail.com>
2023-12-03 18:23:04 +0900

    [fix] Code 대부분 수정
    - Card 가로값 fixed 였던거 수정
    - Section 별로 구조체 분리
    - Padding 적용되어있던거 수정

--- Cuckoo_swiftUI/Cuckoo/Cuckoo/Views/MainView.swift
+++ Cuckoo_swiftUI/Cuckoo/Cuckoo/Views/MainView.swift
@@ -7,17 +7,13 @@
 
 import SwiftUI
 
-let paddingView = UIView(frame: CGRect(x:0,y:0, width:30,height:0))
 
 struct MainView: View {
     
     //title 수정용
-    @State private var text = "Initial Text"
-    @State private var isEditing = false
     @State private var isPresentingMemoSheet = false
     @State private var newMemoTitle = ""
     @State private var newMemoDetails = ""
-    @State private var searchContent: String = "" // 사용자가 입력할 내용을 저장할 상태 변수입니다.
     
     //메모들을 위한 item
     let items: [Item] = [
@@ -37,113 +33,34 @@ struct MainView: View {
     var body: some View {
         NavigationView{
             VStack(spacing:10){
-                HStack{
-                    Spacer()
-                    NavigationLink(destination:SettingView()){
-                        Image(systemName:"gearshape.circle.fill")
-                            .resizable()
-                            .frame(width: 45,height: 45)
-                            .foregroundColor(.gray)
-                            .padding(.trailing,10)
-                    }
-                    NavigationLink(destination: AlarmView()){
-                        Image(systemName:"bell.circle.fill")
-                            .resizable()
-                            .frame(width: 45,height: 45)
-                            .foregroundColor(.gray)
-                    }
-                }
-                
-                
-                //Title
-                VStack{
-                    HStack(spacing:0) {
-                        if isEditing {
-                            TextField("Edit text", text: $text)
-                                .border(Color.gray, width: 1)
-                                .padding()
-                        } else {
-                            VStack(alignment: .leading,spacing: 0){
-                                Text("득수의 메모장")
-                                    .font(.system(size: 30, weight: .heavy))
-                                    .lineSpacing(130)
-                                    .kerning(-0.03)
-                                    .multilineTextAlignment(.leading)
-                                Text("\(itemCount)개의 메모")
-                                    .font(.system(size:12, weight: .regular))
-                                    .lineSpacing(52)
-                                    .kerning(0)
-                                    .multilineTextAlignment(.leading)
-                                    .foregroundColor(Color(red: 0.70, green: 0.70, blue: 0.70))
-                            }
-                            
-                        }
-                        Spacer()
-                    }
-                }
+                // Header
+                MainViewHeader(itemCount: itemCount)
                 Spacer()
-                CardContent {
-                    // TextEditor의 스크롤 가능한 영역 설정
-                    HStack(spacing: 0){
-                        TextField("", text: $searchContent, axis: .vertical)
-                            .font(.system(size: 12, weight: .medium))
-                            .padding(.leading, 25)
-                            .padding(10)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 36, alignment: .top) // 가로길이 고정을 위해 최소 너비를 0, 최대 너비를 무한으로 설정합니다.
-                            .background(.black.opacity(0.1)) // 배경색 설정
-                            .cornerRadius(10) // 코너 반경 설정
-                        
-                        
-                        
-                    }
-                    .overlay(Image(systemName: "magnifyingglass")
-                        .padding(.leading, 10)
-                        .foregroundColor(.gray)
-                             , alignment: .leading)
-                    
-                    
-                }
                 
-                Spacer()
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 10) {
-                        ForEach(0..<10) { index in
-                            CardContent {
-                                HStack {
-                                    TypeBubble("메모", "#b2b2b2")
-                                    TypeBubble("기록", "#b2b2b2")
-                                }
-                            }
-                        }
-                    }
-                }
+                // Search Bar & Tag
+                MainViewSearchFilter()
                 
-                Spacer()
+                // Body
                 ScrollView(){
                     ForEach(items, id: \.title) { item in
-                        MainContainerView(title: item.title, detail: item.detail, tag: item.tag, timeAgo: item.timeAgo, memoURL: item.memoURL, imageName: item.imageName)
-                            .padding(.bottom, 16)
+                        MainContainerView(
+                            title: item.title,
+                            detail: item.detail,
+                            tag: item.tag,
+                            timeAgo: item.timeAgo,
+                            memoURL: item.memoURL,
+                            imageName: item.imageName
+                        ).padding(.bottom, 15)
                     }
-                }
+                }.padding(.top, 10)
                 .scrollIndicators(.hidden)
                 .overlay(
-                    // Floating Action Button
-                    NavigationLink(destination:AddMemoView()){
-                        Image(systemName: "pencil")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(.white)
-                            .padding()
-                    }
-                        .background(Color.gray)
-                        .clipShape(Circle())
-                        .padding()
-                        .padding(.bottom, 16) // Adjust the bottom padding as needed
-                    , alignment: .bottomTrailing
+                    AddMemoFloatingButton(),
+                    alignment: .bottomTrailing
                 )
-            }
-        }.padding(.horizontal, 30)
+                
+            }.padding(.horizontal, 30)
+        }
     }
 }
 
@@ -151,5 +68,123 @@ struct MainView: View {
 struct MainView_PreViews: PreviewProvider {
     static var previews: some View {
         MainView()
+    }
+}
+
+
+// Components
+
+struct MainViewHeader: View {
+    var itemCount: Int
+    
+    @State private var text = "Initial Text"
+    @State private var isEditing = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                NavigationLink(destination:SettingView()){
+                    Image(systemName:"gearshape.circle.fill")
+                        .resizable()
+                        .frame(width: 45,height: 45)
+                        .foregroundColor(.gray)
+                        .padding(.trailing,10)
+                }
+                NavigationLink(destination: AlarmView()){
+                    Image(systemName:"bell.circle.fill")
+                        .resizable()
+                        .frame(width: 45,height: 45)
+                        .foregroundColor(.gray)
+                }
+            }
+            Spacer()
+        }.frame(height: 80)
+        
+        
+        //Title
+        VStack{
+            HStack(spacing:0) {
+                if isEditing {
+                    TextField("Edit text", text: $text)
+                        .border(Color.gray, width: 1)
+                        .padding()
+                } else {
+                    VStack(alignment: .leading,spacing: 0){
+                        Text("득수의 메모장")
+                            .font(.system(size: 30, weight: .heavy))
+                            .lineSpacing(130)
+                            .kerning(-0.03)
+                            .multilineTextAlignment(.leading)
+                        Text("\(itemCount)개의 메모")
+                            .font(.system(size:12, weight: .regular))
+                            .lineSpacing(52)
+                            .kerning(0)
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(Color(red: 0.70, green: 0.70, blue: 0.70))
+                    }
+                    
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct MainViewSearchFilter: View {
+    @State private var searchContent: String = "" // 사용자가 입력할 내용을 저장할 상태 변수입니다.
+    
+    var body: some View {
+        // TextEditor의 스크롤 가능한 영역 설정
+        HStack(spacing: 0){
+            TextField("", text: $searchContent, axis: .vertical)
+                .font(.system(size: 12, weight: .medium))
+                .padding(.leading, 25)
+                .padding(10)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 36, alignment: .top) // 가로길이 고정을 위해 최소 너비를 0, 최대 너비를 무한으로 설정합니다.
+                .background(.black.opacity(0.1)) // 배경색 설정
+                .cornerRadius(10) // 코너 반경 설정
+            
+            
+            
+        }
+        .overlay(Image(systemName: "magnifyingglass")
+            .padding(.leading, 10)
+            .foregroundColor(.gray)
+                 , alignment: .leading)
+        
+        
+        Spacer()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 10) {
+                ForEach(0..<10) { index in
+                    CardContent {
+                        HStack {
+                            TypeBubble("메모", "#b2b2b2")
+                            TypeBubble("기록", "#b2b2b2")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddMemoFloatingButton: View {
+    var body: some View {
+        // Floating Action Button
+        NavigationLink(destination:AddMemoView()){
+            Image(systemName: "pencil")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                .padding()
+        }
+            .background(Color.gray)
+            .clipShape(Circle())
+            .padding()
+            .padding(.bottom, 16) // Adjust the bottom padding as needed
+        
     }
 }
