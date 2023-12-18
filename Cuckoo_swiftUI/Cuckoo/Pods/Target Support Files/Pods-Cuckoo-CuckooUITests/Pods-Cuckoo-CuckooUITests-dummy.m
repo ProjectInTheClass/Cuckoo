7893b55a38a8d4895b9539e6f65582fbe17b6893
VYWL <dksu40@gmail.com>
2023-12-18 12:46:01 +0900

    [fix] pod 의존성 삭제

--- Cuckoo_swiftUI/Cuckoo/Pods/Target Support Files/Pods-Cuckoo-CuckooUITests/Pods-Cuckoo-CuckooUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cuckoo_CuckooUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Cuckoo_CuckooUITests
-@end
--- Cuckoo_swiftUI/Cuckoo/Pods/Target Support Files/Pods-CuckooTests/Pods-CuckooTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CuckooTests : NSObject
-@end
-@implementation PodsDummy_Pods_CuckooTests
-@end
