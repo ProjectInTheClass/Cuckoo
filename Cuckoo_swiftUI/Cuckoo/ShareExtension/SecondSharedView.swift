fff940c752966c9476ba9b54092aa6094d875e19
2021056317 <myday2day@hanyang.ac.kr>
2023-12-18 22:39:35 +0900

    [fix] 버튼 크기, 색 / secondSharedView 카드 음영

--- Cuckoo_swiftUI/Cuckoo/ShareExtension/SecondSharedView.swift
+++ Cuckoo_swiftUI/Cuckoo/ShareExtension/SecondSharedView.swift
@@ -11,24 +11,21 @@ import SwiftSoup
 import CoreData
 
 struct SecondSharedView: View {
+    
     @Binding var onClose: () -> Void // 창을 닫기 위해서 있음
     @Binding var newMemo: MemoEntity?
     
     @State private var registrationCompleted: Bool = false
     
     var body: some View {
-        
-        
-        VStack(spacing: 20){
-            
+        VStack(spacing: 20) {
             Spacer()
-            
+
             Text("메모 등록 완료!")
                 .font(.system(size: 20, weight: .bold))
                 .foregroundColor(Color(red: 0, green: 0, blue: 0).opacity(0.80))
-            
-            
-            VStack{
+
+            VStack {
                 if let _memo = newMemo {
                     MainContainerView(
                         memo: _memo,
@@ -41,27 +38,42 @@ struct SecondSharedView: View {
                     )
                     .frame(maxHeight: 100)
                     .padding(.horizontal, 30)
+                    .padding(.vertical, 30)
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 5)
                 }
-            
             }
-            
-            
-            HStack(spacing: 30){
-                
-                    Spacer()
-                Button("확인"){
+
+            HStack(spacing: 30) {
+                Spacer()
+
+                Button("확인") {
                     onClose()
                 }
-                
-                
-                Button("앱에서 보기"){
-                    var a = Link("Open in Browser", destination: URL(string : "Cuckoo://a")!)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 150, height: 50) // Set a fixed size for the button
+                .background(Color.cuckooViolet)
+                .cornerRadius(10)
+
+                Button("앱에서 보기") {
+                    // Handle the action for "앱에서 보기"
+                    // TODO : "http://Cuckoo/a" 로 연결 , 단, share extension이기 때문에 UIApplication.shared은 쓰지 못한다.
                 }
-                
-                    Spacer()
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 150, height: 50) // Set the same fixed size for the button
+                .background(Color.cuckooPurple)
+                .cornerRadius(10)
+
+                Spacer()
             }
-            
+            .padding(.horizontal, 30)
+            .frame(maxWidth: .infinity)
+
             Spacer()
         }
+        .padding(20)
     }
 }
