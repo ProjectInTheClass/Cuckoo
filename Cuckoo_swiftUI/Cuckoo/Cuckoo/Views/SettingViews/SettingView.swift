da404ff3e6f7aeb85be55e35255b851ed6abe246
2021056317 <myday2day@hanyang.ac.kr>
2023-12-23 22:32:29 +0900

    [fix] 문의하기, 개인정보처리방침 추가

--- Cuckoo_swiftUI/Cuckoo/Cuckoo/Views/SettingViews/SettingView.swift
+++ Cuckoo_swiftUI/Cuckoo/Cuckoo/Views/SettingViews/SettingView.swift
@@ -36,13 +36,17 @@ struct SettingView: View {
                 VStack(spacing: 0) {
                     SettingListView(title: "태그 관리") { SettingTagView() }
                     SettingListView(title: "알람 주기/프리셋 설정") { SettingAlarmPresetView() }
-//                    SettingListView(title: "기타 설정") { KitSettingsView() }
+                    //                    SettingListView(title: "기타 설정") { KitSettingsView() }
                 }
                 
                 VStack(spacing: 0) {
                     
-                    SettingListView(title: "개인정보 처리 방침") { LicenseInformationView() }
-                    SettingListView(title: "문의 하기") { LicenseInformationView() }
+                    SettingListView(title: "개인정보 처리 방침") {
+                        LicenseInformationView()
+                    }
+                    SettingListView(title: "문의 하기") {
+                        RequestView()
+                    }
                 }
                 
                 Spacer()
@@ -106,17 +110,60 @@ struct SettingListView<T: View>: View {
     }
 }
 
+struct LicenseInformationView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HeaderView(title: "개인정보 처리 방침")
+                .frame(height: 60)
+            
+            Spacer() // 상단에 공간 추가
+            
+            HStack{
+                Spacer()
+                Link(destination: URL(string: "https://quark-hydrant-899.notion.site/82c449c89c7341b683bcedd37b9abab6")!) {
+                    HStack {
+                        Text("개인정보 처리 방침 이동 link")
+                    }
+                    .foregroundColor(.blue)
+                }
+                Spacer()
+            }
+            
+            
+            Spacer() // 중간에 공간 추가
+        }
+        .navigationBarHidden(true)
+    }
+}
 
+struct RequestView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HeaderView(title: "문의하기")
+                .frame(height: 60)
+            
+            Spacer() // 상단에 공간 추가
+            
+            HStack{
+                Spacer()
+                Link(destination: URL(string: "https://quark-hydrant-899.notion.site/82c449c89c7341b683bcedd37b9abab6")!) {
+                    HStack {
+                        Text("문의하기 link")
+                    }
+                    .foregroundColor(.blue)
+                }
+                Spacer()
+            }
+            
+            Spacer() // 중간에 공간 추가
+        }
+        .navigationBarHidden(true)
+    }
+}
 
 struct KitSettingsView: View {
     var body: some View {
         Text("Kit Settings")
-    }
-}
-
-struct LicenseInformationView: View {
-    var body: some View {
-        Text("License Information")
     }
 }
 
